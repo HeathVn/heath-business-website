ce7ac8dd9416dd7833327f99bca8dacd5605e2b5
Heathvonn Styles <hvonn1039@gmail.com>
2021-10-16 01:02:33 -0400

    changed server port and added host

--- server/index.js
+++ server/index.js
@@ -16,12 +16,13 @@ app.use(cors());
 app.use('/services', servicesRoutes);
 
 const URL = "mongodb+srv://hstyles:Spikenard@cluster0.neei7.mongodb.net/hvBusiness";
-const PORT = process.env.PORT || 5000
+//const PORT = process.env.PORT || 5000
 //connect to mongoose
-
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
 
 mongoose.connect(URL, {useNewUrlParser:true, useUnifiedTopology:true})
-  .then(()=> app.listen(PORT, ()=> console.log("Server running on port : ${PORT} ")))
+  .then(()=> app.listen(port, host, ()=> console.log("Server running on port : ${PORT} ")))
   .catch((error)=> console.log(error.message));
 
 //mongoose.set('useFindAndModify',false);
