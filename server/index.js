0eeab11eeb912e77d0ca3a578658b1f8fc394b06
Heathvonn Styles <hvonn1039@gmail.com>
2021-10-16 02:10:00 -0400

    package.json added

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import servicesRoutes from './server/routes/services.js'
+import servicesRoutes from './routes/services.js'
 
 const app = express();
 
@@ -13,16 +13,18 @@ app.use(express.json({limit: "30mb", extended: true}));
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
+
+
 app.use('/services', servicesRoutes);
 
 const URL = "mongodb+srv://hstyles:Spikenard@cluster0.neei7.mongodb.net/hvBusiness";
 //const PORT = process.env.PORT || 5000
 //connect to mongoose
-const host = '0.0.0.0';
-const port = process.env.PORT || 3000;
+//const host = '0.0.0.0';
+const port = process.env.PORT || 5000;
 
 mongoose.connect(URL, {useNewUrlParser:true, useUnifiedTopology:true})
-  .then(()=> app.listen(port, host, ()=> console.log("Server running on port : ${port} ")))
+  .then(()=> app.listen(port, ()=> console.log("Server running on port : ${port} ")))
   .catch((error)=> console.log(error.message));
 
 //mongoose.set('useFindAndModify',false);
